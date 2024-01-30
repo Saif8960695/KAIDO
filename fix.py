5cfd132ff9d5629e52ef73d3853ebf6e8e6ba4e5
Mohammad ali <108490282+Saif8960695@users.noreply.github.com>
2024-01-30 22:37:41 +0600

    Update fix.py

--- fix.py
+++ fix.py
@@ -5,3 +5,4 @@ system('rm -rf /data/data/com.termux/files/usr/lib/python3.11/site-packages/requ
 system('pkg uninstall python -y;pkg install python -y;pip uninstall requests chardet urllib3 idna certifi -y;pip install chardet urllib3 idna certifi requests')
 system('clear')
 print("MAYBE FIXED RUN TOOLS AGAIN")
+fuck nill
